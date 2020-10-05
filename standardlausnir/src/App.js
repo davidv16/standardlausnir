6a98592f2b4c842813ed070a8f9863c53b14ec0f
davidv16 <davidv16@ru.is>
2020-10-05 21:21:52 +0000

    move between computers

--- standardlausnir/src/App.js
+++ standardlausnir/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDom from "react-dom"
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Temporary from './components/Temporary'
+import { Router, Route, Switch } from 'react-router-dom';
+
+// styles
 import './style.css'
+import "./assets/scss/material-kit-react.scss?v=1.4.0";
+
+// pages
+import Temporary from './views/Temporary/Temporary'
+import LandingPage from './views/LandingPage/LandingPage'
 
 const App = () => (
   <Switch>
     <Route exact path='/' component={Temporary} />
+    <Route exact path='/LandingPage' component={LandingPage} />
   </Switch>
 );
 
